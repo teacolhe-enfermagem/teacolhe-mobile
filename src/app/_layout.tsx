930a1d47f7bacb4b97a2a4f2cc0f2ce8b8d99301
williamdev20 <williamalvs45@gmail.com>
2026-05-16 18:23:58 -0300

    chore: adicionar fonte Inter e configurar os _layout.tsx dentro da pasta e subpastas de apps/

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,29 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold } from "@expo-google-fonts/inter";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
 import "@/global.css";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold
+  })
+
+  useEffect(() => {
+    NavigationBar.setButtonStyleAsync("dark");
+  }, []);
+  
+  if (!fontsLoaded) return null;
+  
+  return (
+    <>
+      <StatusBar style="dark" />
+
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  )
 }
