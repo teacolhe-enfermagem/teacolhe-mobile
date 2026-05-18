41e2a8f48d83d3aca8a35f946f2a8b9a080a4fb0
williamdev20 <williamalvs45@gmail.com>
2026-05-18 10:47:13 -0300

    feat: adicionar context e provider de autenticação

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,18 @@
 import  HomeScreen  from "@/src/features/home/screens/HomeScreen";
 import { Redirect } from "expo-router";
+import { useAuth } from "../context/auth/AuthContext";
+import { View, ActivityIndicator } from "react-native";
 
 export default function Index() {
-  return <Redirect href="/auth/login" />;
-  
-  return <HomeScreen />;
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator />
+      </View>
+    )
+  }
+
+  return isAuthenticated ? <Redirect href="/(protected)/patient/select-level" /> : <HomeScreen />;
 }
