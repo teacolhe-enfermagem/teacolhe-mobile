4b2c8e64e24d1dea70a2f83b5fd9015cf4b869fe
williamdev20 <williamalvs45@gmail.com>
2026-05-17 22:22:06 -0300

    fix: mover tela de login para LoginScreen.tsx e criar arquivo login.tsx na pasta app para renderizar a tela

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import  HomeScreen  from "@/src/features/home/screens/HomeScreen";
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/auth/signup" />;
+  return <Redirect href="/auth/login" />;
   
   return <HomeScreen />;
 }
