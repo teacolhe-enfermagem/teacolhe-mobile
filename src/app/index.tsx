e7432a93eb63eb34e67cb4f0b546de57b592f84b
williamdev20 <williamalvs45@gmail.com>
2026-05-17 22:18:29 -0300

    fix: mover tela de cadastro para SignupScreen.tsx e criar arquivo signup.tsx na pasta app para renderizar a tela

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import  HomeScreen  from "@/src/features/home/screens/HomeScreen";
+import { Redirect } from "expo-router";
 
 export default function Index() {
   return <Redirect href="/auth/signup" />;
