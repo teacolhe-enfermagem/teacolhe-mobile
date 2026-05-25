ab479cf763c7758e3e0d42ed3b6c6fda9dfa4634
williamdev20 <williamalvs45@gmail.com>
2026-05-24 22:58:44 -0300

    feat: consumir a rota /user/me para pegar informações do usuário

--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -18,16 +18,25 @@ type AuthResponse = {
   token_type: string;
 };
 
+type UserResponse = {
+  name: string;
+};
+
 export const authService = {
   async login(payload: LoginPayload): Promise<AuthResponse> {
     const { data } = await api.post<AuthResponse>("/auth/login", payload);
-    
     return data;
   },
 
   async register(payload: RegisterPayload): Promise<AuthResponse> {
     const { data } = await api.post<AuthResponse>("/auth/register", payload);
-
     return data;
   },
+
+  async getUser(token: string): Promise<UserResponse> {
+  const { data } = await api.get("/user/me", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return data;
+},
 };
